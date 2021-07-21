dea2ea4bde3c2450f43b130f5018deba5ba6c0ca
Divesh Keswani <diveshkswn@gmail.com>
2021-07-21 16:59:52 +0530

    21-07-2021

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,28 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import styles from '../styles/Home.module.css';
 import Sidebar from '../components/Sidebar';
 import { useAuth } from '../Context/Authcontext';
+import { projectFirestore, timestamp } from '../firebase';
 
 export default function Home() {
   const route = useRouter();
   const { currentUser } = useAuth();
+  const demo = 'Divesh';
 
-  if (!currentUser) { route.push('/login'); return null; }
+  // Updating/Adding user information on every change in currentUser of firebase
+  useEffect(() => {
+    console.log('Updating user');
+    projectFirestore.collection('users').doc(currentUser.uid).set({
+      email: currentUser.email,
+      lastSeen: timestamp(),
+      photoURL: currentUser.photoURL,
+
+    }, { merge: true });
+  }, [currentUser]);
+
+  if (!currentUser) { route.push('/login'); return ''; }
 
   return (
     <>
