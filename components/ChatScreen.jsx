cd5d17af56fca2a1bbab98ce314b890617aafdf9
Divesh Keswani <diveshkswn@gmail.com>
2021-07-23 23:27:42 +0530

    Chat Screen layout completed

--- components/ChatScreen.jsx
+++ components/ChatScreen.jsx
@@ -1,9 +1,35 @@
-import styles from '../styles/ChatScreen.module.css';
+import { useRouter } from 'next/router';
+import { Avatar, IconButton } from '@material-ui/core';
+import MoreVert from '@material-ui/icons/MoreVert';
+import { AttachFile } from '@material-ui/icons';
 
-export default function ChatScreen() {
+import styles from '../styles/ChatScreen.module.css';
+import { useAuth } from '../Context/Authcontext';
+
+export default function ChatScreen(props) {
+  const { chat, messages } = props;
+  const { currentUser } = useAuth();
+  const router = useRouter();
+
   return (
     <div className={styles.ChatScreenContainer}>
-      <h1>Chat Screen</h1>
+      <div className={styles.ChatHeader}>
+        <Avatar />
+        <div className={styles.HeaderInfo}>
+
+          <h3>User Email</h3>
+          <p>Last Seen...</p>
+        </div>
+
+        <div className="HeaderIcons">
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+          <IconButton><AttachFile /></IconButton>
+
+        </div>
+
+      </div>
     </div>
   );
 }
--- pages/chat/[id].jsx
+++ pages/chat/[id].jsx
@@ -4,8 +4,11 @@ import Sidebar from '../../components/Sidebar';
 import styles from '../../styles/Chat.[id].module.css';
 import { useAuth } from '../../Context/Authcontext';
 import ChatScreen from '../../components/ChatScreen';
+import { projectFirestore } from '../../firebase';
+import getRecipientEmail from '../../utils/getRecipientEmail';
 
-export default function ChatWithId() {
+export default function ChatWithId(props) {
+  const { messages, chat } = props;
   const { currentUser } = useAuth();
   const router = useRouter();
   if (!currentUser) { router.push('/login'); return ''; }
@@ -13,30 +16,44 @@ export default function ChatWithId() {
   return (
     <div className={styles.Container}>
       <Head>
-        <title>Chat</title>
+        <title>{`Chat with ${getRecipientEmail(chat.users, currentUser)}`}</title>
       </Head>
       <div className={styles.SidebarContainer}>
         <Sidebar />
       </div>
 
       <div className={styles.chatContainer}>
-        <h2>{router.query.id}</h2>
-        <ChatScreen />
+
+        <ChatScreen chat={chat} messages={messages} />
       </div>
 
     </div>
   );
 }
 
-// export async function getStaticProps(context) {
-//   return {
-//     props: { id: context.params.id },
-//   };
-// }
+export async function getServerSideProps(context) {
+  const ref = projectFirestore.collection('chats').doc(context.params.id);
+  const responseMessages = await ref.collection('messages').orderBy('timestamp', 'asc').get();
 
-// export async function getStaticPaths() {
-//   return {
-//     paths: [{ params: { id: '1' } }],
-//     fallback: 'blocking',
-//   };
-// }
+  // Prepare messages on server.
+  const messages = responseMessages.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })).map((messages) => ({
+    ...messages,
+    timestamp: messages.timestamp.toDate().getTime(),
+  }));
+
+  // Prepare the chats
+  const chatRes = await ref.get();
+  const chat = {
+    id: chatRes.id,
+    ...chatRes.data(),
+  };
+  return {
+    props: {
+      messages: JSON.stringify(messages),
+      chat,
+    },
+  };
+}
