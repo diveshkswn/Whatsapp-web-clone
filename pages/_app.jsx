fe05add1912f08e656d3e0008a3e6f96c123e07e
Divesh Keswani <diveshkswn@gmail.com>
2021-08-04 21:52:42 +0530

    Loading bar top on serverSideProps changes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,40 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.css';
 
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { LinearProgress } from '@material-ui/core';
 import Layout from '../components/Layout';
 import AuthProvider from '../Context/Authcontext';
 
 function MyApp({ Component, pageProps }) {
+  const [topLoading, setTopLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    function handleRouteChangeStart() {
+      setTopLoading(false);
+    }
+    function handleRouteChangeEnd() {
+      setTopLoading(true);
+    }
+
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on('routeChangeComplete', handleRouteChangeEnd);
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method:
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off('routeChangeComplete', handleRouteChangeEnd);
+    };
+  }, [router]);
+
   return (
     <AuthProvider>
       <Layout>
         <div className="themeContainer">
+          <LinearProgress hidden={topLoading} />
+
           <Component {...pageProps} />
         </div>
 
